1b503b5017a827694b5793073be19655a2713537
Miguel Jimenez <miguel1315@gmail.com>
2020-10-12 23:26:49 +0200

    Add new classes Report and Structure

--- project/src/classes/file.py
+++ project/src/classes/file.py
@@ -1,9 +1,11 @@
 from dataclasses import dataclass
+from typing import Literal
 
+PDF_CONTENT_TYPE = 'application/pdf'
+TXT_CONTENT_TYPE = 'text/plain'
 
 @dataclass
 class File:
     title: str
-    content_type: str
+    content_type: Literal[PDF_CONTENT_TYPE, TXT_CONTENT_TYPE]
     content: str
-
---
+++ project/src/classes/structure.py
@@ -0,0 +1,8 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Structure:
+    main_separator: str
+    header_separator: str
+    header_key_separator: str
