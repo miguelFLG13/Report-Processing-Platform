5ec4926c4a607e0cc75b4225ab8eda5d32f23103
Miguel Jimenez <miguel1315@gmail.com>
2020-10-12 23:41:48 +0200

    Add endpoint to process a txt file and convert to a report

--- project/src/api/services.py
+++ project/src/api/services.py
@@ -1,7 +1,16 @@
 from django.core.files.uploadedfile import InMemoryUploadedFile
 
+from classes.file import PDF_CONTENT_TYPE, TXT_CONTENT_TYPE
+
+
 def is_pdf_file(file: InMemoryUploadedFile):
     """ Service to check if a InMemoryUploadedFile is a Pdf """
     content_type = file.content_type
 
-    return True if content_type == 'application/pdf' else False
+    return True if content_type == PDF_CONTENT_TYPE else False
+
+def is_plain_text_file(file: InMemoryUploadedFile):
+    """ Service to check if a InMemoryUploadedFile is a txt """
+    content_type = file.content_type
+
+    return True if content_type == TXT_CONTENT_TYPE else False
--- project/src/api/urls.py
+++ project/src/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from api.views import PdfToTextAPIView
+from api.views import PdfToTextAPIView, ProcessReportAPIView
 
 
 urlpatterns = [
     path('pdf-to-text/', PdfToTextAPIView.as_view(), name='pdf_to_text'),
+    path('process-report/', ProcessReportAPIView.as_view(), name='process_report'),
 ]
